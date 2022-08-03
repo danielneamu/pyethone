41cec5806bb400e5ad860244c8ba6a7dc4be3746
danielneamu <danielneamu@outlook.com>
2022-08-03 17:35:24 +0300

    testt commit

--- python.py
+++ python.py
@@ -1,10 +1,25 @@
-count = 0
-
-my_string = "Programiz"
-my_char = "r"
-
-for i in my_string:
-    if i == my_char:
-        count += 1
-
-print(count)
+# Python3 program to find a list of uncommon words
+  
+# Function to return all uncommon words
+def UncommonWords(A, B):
+  
+    # count will contain all the word counts
+    count = {}
+      
+    # insert words of string A to hash
+    for word in A.split():
+        count[word] = count.get(word, 0) + 1
+      
+    # insert words of string B to hash
+    for word in B.split():
+        count[word] = count.get(word, 0) + 1
+  
+    # return required list of words
+    return [word for word in count if count[word] == 1]
+  
+# Driver Code
+A = "Geeks for Geeks"
+B = "Learning from Geeks for Geeks"
+  
+# Print required answer
+print(UncommonWords(A, B))
