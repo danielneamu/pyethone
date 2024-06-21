dd946dd32888944b35f31421ed4819383583fb39
Daniel Neamu <danielneamu@gmail.com>
2024-06-21 19:34:33 +0300

    comit

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,14 @@
 #!/usr/lib/python3
-print ("This line will be printed.")
+#  print ("This line will be printed.")
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<h1>Hello world!</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
